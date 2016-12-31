94dfdfa1beb24249867095050572833f9b5f4dcd
liuyakun <happy_lucky52@163.com>
2016-12-31 17:11:43 +0800

    chapter 5

---
+++ chapter5/page182/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+unsigned long long Factor(int n);
+unsigned long long Factor1(int n);
+
+int main()
+{
+	for(int i = 0; i < 10; ++i)
+	{
+		cout << i << "! = " << Factor(i) << endl;
+		cout << i << "! = " << Factor1(i) << endl;
+	}
+	return 0;
+}
+
+unsigned long long Factor(int n)
+		
+{
+	if(!n)
+		return 1;
+	else
+		return n * Factor(n - 1);
+}
+
+unsigned long long Factor1(int n)
+{
+	int ret = 1;
+	while(n > 1)
+		ret *= n--;
+
+	return ret;
+}
