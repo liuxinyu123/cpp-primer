9094802423afb2e5f6ac5cc37000f0c3145132a6
liuyakun <happy_lucky52@163.com>
2017-01-17 22:40:47 +0800

    page 350

--- chapter10/page350/main.cpp
+++ chapter10/page350/main.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
-using std::cout;
-using std::endl;
+using namespace std;
+
+void Biggies (vector<string> &words, string::size_type sz, ostream &os = cout, char c = ' ');
+void ElimDups (vector<string> &words);
 
 int main (int argc, char *argv[])
 {
-	int val = 23;
-	auto f = [val] { return val;};
-	auto f2 = [&val] { return val;};
-	val = 1;
+	vector<string> strVec;
+	strVec.push_back ("hello");
+	strVec.push_back ("world");
+	strVec.push_back ("what");
+	strVec.push_back ("are");
+	strVec.push_back ("you");
+	strVec.push_back ("doing");
+	
+	for (const auto &elem : strVec)
+		cout << elem << " ";
+	cout << endl;
+	
+	ElimDups (strVec);
 
-	auto val2 = f ();
-	auto val3 = f2 ();
-	cout << val2 << endl;
-	cout << val3 << endl;
+	for (const auto &elem : strVec)
+		cout << elem << " ";
+	cout << endl;
+	
+	Biggies (strVec, 4);
 
 	return 0;
+}
+
+void ElimDups (vector<string> &words)
+{
+	sort (words.begin (), words.end ());
+	auto pos = unique (words.begin (), words.end ());
+	words.erase (pos, words.end ());
+	stable_sort (words.begin (), words.end (), [] (const string &str1, const string &str2)
+					{ return str1.size () < str2.size ();});
+}
+
+void Biggies (vector<string> &words, string::size_type sz, ostream &os, char c)
+{
+	auto pos = find_if (words.begin (), words.end (), [sz] (const string &s) 
+					{ return s.size () >= sz;});
+	auto count = words.end () - pos;
+	cout << count << ((count > 1) ? " words" : " word") << " of length " << sz << " or longer" 
+			<< endl;
+
+	for_each (pos, words.end (), [&os, c] (const string &s)
+					{ os << s << c;});
+	cout << endl;
 }
