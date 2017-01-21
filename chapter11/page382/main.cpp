f3ea598791449d603a70fba3d7afef906f84962a
liuyakun <happy_lucky52@163.com>
2017-01-21 09:30:05 +0800

    page 382

--- chapter11/page382/main.cpp
+++ chapter11/page382/main.cpp
@@ -6,15 +6,20 @@ using namespace std;
 
 int main (int argc, char *argv[])
 {
-	map<string, int> word_count = { {"world", 54} }; 
+	map<string, int> word_count = { {"world", 54}, {"hello", 23}, {"what", 21}, {"are", 45},
+			{"you", 12}, {"doing", 8}	}; 
 	
 	auto it = word_count.begin ();
 	cout << it -> first << " " << it -> second << endl;
 
 	//it -> first = "hello";
 	++ it -> second;
+	while (it != word_count.end ())
+	{
+		cout << it -> first << " " << it -> second << endl;
+		++it;	
+	}
 
-	cout << it -> first << " " << it -> second << endl;
 	return 0;
 	
 }
