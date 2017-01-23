d4f6abd9fb76ebd04ba94422e22bb2e0d796c5f9
liuyakun <happy_lucky52@163.com>
2017-01-23 16:26:15 +0800

    page 384

--- chapter11/page384/main.cpp
+++ chapter11/page384/main.cpp
@@ -8,4 +8,24 @@ int main (int argc, char *argv[])
 {
 	vector<int> iVec;
 
+	for (int i = 0; i < 9; i = i + 2)
+	{
+		iVec.push_back (i);
+		iVec.push_back (8 - i);
+	}
+
+	set<int> set1;
+	set1.insert (iVec.begin (), iVec.end ());
+
+	for (const auto & elem : set1)
+		cout << elem << " ";
+	cout << endl;
+
+	set1.insert ({12, 12, 34, 12, 21, 21});
+	for (const auto & elem : set1)
+		cout << elem << " ";
+	cout << endl;
+
+	return 0;
+
 }
