dee1c3d3178ec4e62e5364f3385d7803905d7ce3
liuyakun <happy_lucky52@163.com>
2017-01-26 18:58:34 +0800

    page 402

--- chapter12/page402/main.cpp
+++ chapter12/page402/main.cpp
@@ -8,6 +8,15 @@ using namespace std;
 int main (int argc, char *argv[])
 {
 	auto p1 = make_shared<int> (11);
-	auto p2 (p1);
+	//{
+		auto p2 (p1);
+	//}
+	auto p3 = make_shared <string> ();
+
+	cout << p1.use_count () << endl;
+	cout << p2.use_count () << endl;
+	cout << p3.use_count () << endl;
+
+	return 0;
 
 }
