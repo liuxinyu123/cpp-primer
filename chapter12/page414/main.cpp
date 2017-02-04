825c32211eb48a8e55335d93ac0fac94a4db80d8
liuyakun <happy_lucky52@163.com>
2017-02-04 10:31:22 +0800

    page 414

--- chapter12/page414/main.cpp
+++ chapter12/page414/main.cpp
@@ -1,6 +1,9 @@
 #include <new>
 #include <iostream>
 #include <memory>
+#include <string>
+
+using std::string;
 
 int main (int argc, char *argv[])
 {
@@ -14,6 +17,17 @@ int main (int argc, char *argv[])
 	int i = *q;
 	
 	std::cout << *p << std::endl;
+
+	std::shared_ptr<string> p2 (new string ("hello"));
+	std::shared_ptr<string> p3 (p2);
+
+	if (!p2.unique ())
+		p2.reset (new string (*p2));
+	*p2 += "world";
+
+	std::cout << *p2 << std::endl;
+	std::cout << *p3 << std::endl;
+
 
 	return 0;
 }
