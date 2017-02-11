963b98b43a58db07b7bb3bd847a817395917f484
liuyakun <happy_lucky52@163.com>
2017-02-11 16:32:47 +0800

    page 447

--- chapter13/page447/main.cpp
+++ chapter13/page447/main.cpp
@@ -9,6 +9,15 @@ class HasPtr
 public:
 		HasPtr (const std::string &s = std::string ())
 				:ps (new std::string (s)), i (0)  {}
+		HasPtr (const HasPtr& hp) 
+				:ps (new std::string (*hp.ps)), i (hp.i) {}
+		HasPtr& operator= (const HasPtr& hp)
+		{
+			ps = new std::string (*hp.ps);
+			i = hp.i;
+
+			return *this;
+		}
 		~HasPtr ()
 		{
 			delete ps;
@@ -23,10 +32,10 @@ HasPtr f (HasPtr hp);
 int main (int argc, char *argv[])
 {
 	HasPtr p1("hello");
-	//HasPtr p2 = f (p1);
+	HasPtr p2 = f (p1);
 	//HasPtr p2 (p1);
-	//f (p1);
-	//HasPtr p3 (p1);
+	f (p1);
+	HasPtr p3 (p1);
 
 	return 0;	
 }
