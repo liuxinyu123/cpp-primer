ea73af3f51d993937eabe07337f0694b8c8f1d7c
liuyakun <happy_lucky52@163.com>
2017-02-11 12:18:31 +0800

    page 447

---
+++ chapter13/page447/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <new>
+
+
+class HasPtr
+{
+
+public:
+		HasPtr (const std::string &s = std::string ())
+				:ps (new std::string (s)), i (0)  {}
+		~HasPtr ()
+		{
+			delete ps;
+		}
+private:
+		std::string *ps;
+		int i;
+};
+
+HasPtr f (HasPtr hp);
+
+int main (int argc, char *argv[])
+{
+	HasPtr p1;
+	//HasPtr p2 = f (p1);
+	//HasPtr p2 (p1);
+	return 0;	
+}
+
+HasPtr f (HasPtr hp)
+{
+	HasPtr ret = hp;
+	return ret;
+}
