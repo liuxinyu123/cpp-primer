99ccd4978ab397e26ecc8c21e28e9ab30386343b
liuyakun <happy_lucky52@163.com>
2017-02-11 17:12:40 +0800

    page 451

---
+++ chapter13/page451/main.cpp
@@ -0,0 +1,21 @@
+
+class PrivateCopy
+{
+	
+public:
+		PrivateCopy () = default;
+		~PrivateCopy () = default;
+
+private:
+		PrivateCopy (const PrivateCopy&) {}
+		PrivateCopy& operator= (const PrivateCopy&) {}
+};
+
+int main (int argc, char *argv[])
+{
+	PrivateCopy pc1, pc2;
+	//pc2 = pc1;
+	//privateCopy pc3 = pc2;
+
+	return 0;
+}
