a7451d48f519ae503a8097fb5531382b77664fc6
liuyakun <happy_lucky52@163.com>
2017-02-13 22:55:27 +0800

    page 461

--- chapter13/page461/main.cpp
+++ chapter13/page461/main.cpp
@@ -6,8 +6,6 @@ int main (int argc, char *argv[])
 {
 	Message m1 ("hello");
 	Message m2 ("world");
-	Message m3 = m1;
-	Message m4 (m2);
 
 	Folder f1 ("folder1");
 	Folder f2 ("folder2");
@@ -22,8 +20,19 @@ int main (int argc, char *argv[])
 	m1.PrintFolder ();
 	m2.PrintFolder ();
 
+	Message m3 = m1;
+	Message m4 (m2);
+
+	f1.PrintMessage ();
+	f2.PrintMessage ();
 	m3.PrintFolder ();
 	m4.PrintFolder ();
+	
+	f1.Remove (m2);
+	f2.Remove (m1);
+
+	f1.PrintMessage ();
+	f2.PrintMessage ();
 
 
 	return 0;
