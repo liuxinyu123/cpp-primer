ab47bdb4d13353e3c913ce564d6ca3c683dcd97e
liuyakun <happy_lucky52@163.com>
2017-02-18 11:44:59 +0800

    page 507

---
+++ chapter14/page507/main.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class PrintString 
+{
+
+public:
+		PrintString (std::ostream &o = std::cout, char c = ' ')
+			:os (o), sep (c) {}
+		void operator() (std::string &s) 
+		{
+			os << s << sep;
+		}
+private:
+		std::ostream &os;
+		char sep;
+};
+
+int main (int argc, char *argv[])
+{
+	std::vector<std::string> strVec = {"what", "are", "you", "doing"};
+	
+	PrintString ps;
+
+	for (std::size_t i = 0; i < strVec.size (); ++i)
+		ps (strVec[i]);
+	std::cout << std::endl;
+
+	//PrintString pError (std::cerr, '\n');
+	std::for_each (strVec.begin (), strVec.end (), PrintString (std::cerr, '\n'));
+
+	return 0;
+}
