08de5c9487e6fbe68b0a3dc7864894edfd00e2de
liuyakun <happy_lucky52@163.com>
2017-02-20 21:13:35 +0800

    page 535

---
+++ chapter15/page535/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+class Base
+{
+
+friend std::ostream& operator << (std::ostream &os, const Base &b);
+
+public:
+		Base (std::size_t val = 0)
+			:i (val) {}
+protected:
+		std::size_t i;
+};
+
+class Derived : public Base
+{
+friend std::ostream& operator << (std::ostream &os, const Derived &d);
+public : 
+		Derived (std::size_t val, const std::string &str = "")
+			:Base (val), s (str) {}
+private:
+		std::string s;
+};
+
+std::ostream& operator << (std::ostream &os, const Base &b)
+{
+	os << b.i;
+	return os;
+}
+
+std::ostream& operator << (std::ostream &os, const Derived &d)
+{
+	os << d.i << " "  << d.s;
+	return os;
+}
+
+int main (int argc, char *argv[])
+{
+	Derived d1 (2, "hello");
+	Derived d2 (4, "world");
+	Base b (d1);
+
+	std::cout << b << std::endl;
+	std::cout << d1 << std::endl;
+
+	b = d2;
+
+	std::cout << b << std::endl;
+}
