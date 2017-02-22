5f27e737de13df705fb7f5a57a4a5ac07cb24200
liuyakun <happy_lucky52@163.com>
2017-02-22 23:01:32 +0800

    page 584

--- chapter16/page584/blob.h
+++ chapter16/page584/blob.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <vector>
 #include <string>
+#include <stdexcept>
 
 template <typename T>
 class Blob
@@ -31,6 +32,7 @@ public:
 		{
 			data -> pop_back ();
 		}
+		T& Front () const;
 		T& Back () const;
 		T& operator[] (size_type i) const;
 private:
@@ -38,4 +40,24 @@ private:
 		void Check (size_type i, const std::string &msg) const;
 };
 
+template <typename T>
+Blob:Blob ()
+	:data (nullptr) {}
+
+template <typename T>
+Blob:Blob (std::initializer_list<T> il)
+	:data (new std::vector<T> (il)) {}
+
+template <typename T>
+T& Blob:Back () const
+{
+	
+}
+
+template <typename T>
+void Blob:Check (size_type i, const std::string &s = msg) const
+{
+	if (i >= Size ())	
+		throw std::out_of_range (msg);
+}
 #endif
